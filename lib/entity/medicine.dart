96c69c44f7dcbcba9ee2315fb670bcb22dc3e5d6
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:28:28 +0200

    Add comments

--- lib/entity/medicine.dart
+++ lib/entity/medicine.dart
@@ -1,13 +1,20 @@
 import 'package:floor/floor.dart';
 
+// عنصر الدواء
 @entity
 class Medicine {
+// يحتوي على 
+
+// رقم المواء وهو رقم مميز غير متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
-
+// الاسم
   final String name;
+// الموعد
   final String time;
+// رقم المريض
   final int patientID;
+// ملاحظات
   final String notes;
 
   Medicine(this.id, this.name, this.time, this.patientID, this.notes);
