59f99aa6d5f237629949e2954e395572a3e83c3c
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:22:17 +0200

    Add comments

--- lib/dao/status_dao.dart
+++ lib/dao/status_dao.dart
@@ -1,20 +1,25 @@
 import 'package:floor/floor.dart';
 import 'package:medical_reminder/entity/status.dart';
 
+// جدول خاص بحالات الاشعارات سواء فائتة او لا
 @dao
 abstract class StatusDao {
+// سحب حميع الحالات
   @Query('SELECT * FROM status')
   Future<List<Status>> findAllStatus();
 
   @Query('SELECT * FROM status')
   Stream<List<Status>> findAllStatusAsStream();
 
+// البحث عن خالة معينه عن كريق رقمها
   @Query('SELECT * FROM status WHERE id = :id')
   Future<Status> findStatusById(int id);
 
+// إضافة حالة جديدة
   @insert
   Future<void> insertStatus(Status status);
 
+// مسح حالة من الجدول
   @delete
   Future<void> deletStatus(Status status);
 }
