b5fd73222797c5f626ecaaa3b8d4bdb0edaa9c8b
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:32:51 +0200

    Add comments

--- lib/entity/patient.dart
+++ lib/entity/patient.dart
@@ -1,12 +1,18 @@
 import 'package:floor/floor.dart';
 
+// عنصر المريض
 @entity
 class Patient {
+// يحتوي على
+
+// رقم المريض وهو رقم مميز غير متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
-
+// الاسم
   final String name;
+// صوت الإشعار الهاص به
   final String tone;
+// رقم تليفون الشخص المسؤول عنه
   final String phone;
 
   Patient(this.id, this.name, this.tone, this.phone);
