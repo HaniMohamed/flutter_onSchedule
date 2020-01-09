1cb13e6e434050aeccb7e844a985f5093654ca99
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:46:30 +0200

    Add comments

--- lib/services/shared_prefs.dart
+++ lib/services/shared_prefs.dart
@@ -1,16 +1,21 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+// ملف حفظ بيانات تسجيل الدخول
 class SharedPrefs {
+
+// استدعاء ايميل المستخدم
   Future<String> getSharedEmail() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString("email");
   }
 
+// استدعاء إسم المستخدم
   Future<String> getSharedName() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString("name");
   }
 
+// حفظ اسم وايميل المستخدم
   setSharedStringValue(key, value) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(key, value);
