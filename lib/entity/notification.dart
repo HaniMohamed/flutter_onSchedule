b2b3a8d5d310897fc3dba520054c0ef211d80aeb
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:31:46 +0200

    Add comments

--- lib/entity/notification.dart
+++ lib/entity/notification.dart
@@ -1,11 +1,16 @@
 import 'package:floor/floor.dart';
 
+// عنصر الإشعار
 @entity
 class NotificationEntity {
+// يحتوي على 
+
+// رقم الاشعار وهو رقم مميز غير متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
-
+// الاسم
   final String time;
+// رقم الدواء او الكشف 
   final int targetID;
 
   NotificationEntity(this.id, this.time, this.targetID);
