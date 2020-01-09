1172bc16f47a2816d7cfd3e3995c8a50ce7984ce
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:30:27 +0200

    Add comments

--- lib/entity/missed.dart
+++ lib/entity/missed.dart
@@ -1,12 +1,18 @@
 import 'package:floor/floor.dart';
 
+// عنصر الموعد الفائت
 @entity
 class Missed {
+// يحتوي على
+
+// رقم الموعد الفائت وهو رقم مميز غير متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
-
+// الموعد
   final String time;
+// رقم الدواء الفائت
   final int targetID;
+// حالته سواء تم اخذه ام مازال فائت
   final int done;
 
   Missed(this.id, this.done, this.time, this.targetID);
