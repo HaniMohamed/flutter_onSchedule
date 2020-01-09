1af276c1870345239591e8eed73746783e571942
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:34:18 +0200

    Add comments

--- lib/entity/status.dart
+++ lib/entity/status.dart
@@ -1,11 +1,16 @@
 import 'package:floor/floor.dart';
 
+// عنصر حالة موعد الدواء
 @entity
 class Status {
+// يحتوي على
+
+// رقم الحالة وهو رقم مميز غير متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
-
+// رقم الدواء
   final int targetID;
+// حالته سواء تم اخده ام لا
   final int done;
 
   Status(this.id, this.done, this.targetID);
