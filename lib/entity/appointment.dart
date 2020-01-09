46f3fcc1c11200fb5210a87c6293acecbff7509b
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:26:54 +0200

    Add comments

--- lib/entity/appointment.dart
+++ lib/entity/appointment.dart
@@ -1,13 +1,20 @@
 import 'package:floor/floor.dart';
-
+ // عنصر اموعد الكشف
 @entity
 class Appointment {
+// يحتوي على
+
+// رقم الكشف وهو رقم مميز غسر متكرر
   @PrimaryKey(autoGenerate: true)
   final int id;
 
+// الاسم
   final String name;
+// الموعد
   final String time;
+// رقم المريض
   final int patientID;
+// العنوان
   final String address;
 
   Appointment(this.id, this.name, this.time, this.patientID, this.address);
