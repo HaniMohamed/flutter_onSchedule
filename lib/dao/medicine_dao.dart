461bb8d06a0e668faa0015530a40bb1faf3cb1ba
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:13:58 +0200

    Add comments

--- lib/dao/medicine_dao.dart
+++ lib/dao/medicine_dao.dart
@@ -4,28 +4,35 @@ import 'package:medical_reminder/entity/patient.dart';
 
 @dao
 abstract class MedicineDao {
+// سحب جميع البيانات من جدول الادوية
   @Query('SELECT * FROM medicine')
   Future<List<Medicine>> findAllMedicine();
 
   @Query('SELECT * FROM medicine')
   Stream<List<Medicine>> findAllMedicineAsStream();
 
+// البحث عن دواء في الجدول عن كريق رقمه
   @Query('SELECT * FROM medicine WHERE id = :id')
   Future<Medicine> findMedicineById(int id);
 
   @Query('SELECT * FROM medicine WHERE id = :id')
   Stream<Medicine> findMedicineByIdAsStream(int id);
 
+// البحث عن الادوية الخاصة بمريض معين
   @Query('SELECT * FROM medicine WHERE patientID = :patientID')
   Stream<List<Medicine>> findMedicineByPatientIdAsStream(int patientID);
 
+// البحث عن المريض صاحب دواء معين
   @Query(
       'SELECT * FROM patient p JOIN medicine m ON p.id = m.patientID WHERE m.id = :id')
   Future<Patient> findPatientByMedicineId(int id);
 
+// إضافة دواء جديد
   @insert
   Future<int> insertMedicine(Medicine medicine);
 
+
+// مسح دواء من الجدول
   @delete
   Future<void> deleteMedicine(Medicine medicine);
 }
