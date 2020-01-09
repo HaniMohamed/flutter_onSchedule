1cc965dad8f20bcf940a4a4d7c48a6de59733b16
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:38:15 +0200

    Add comments

--- lib/services/check_missed_medicines.dart
+++ lib/services/check_missed_medicines.dart
@@ -3,6 +3,7 @@ import 'package:medical_reminder/services/database.dart';
 import 'package:medical_reminder/services/schedul_notifiction.dart';
 import 'package:medical_reminder/services/smsSender.dart';
 
+// الخدمة الخاصة بالكشف عن الإشعارات الفائتة
 class CheckMissedNotifications {
   BuildContext context;
 
@@ -11,11 +12,14 @@ class CheckMissedNotifications {
   }
 
   checkMissedNotifs() async {
+// استدعاء الداتابيز وجداول الادوية والمواعيد الفائتة
     var database =
         await $FloorAppDatabase.databaseBuilder('flutter_database.db').build();
     var dao = database.notificationDao;
     var missedDao = database.missedDao;
     var medicineDao = database.medicineDao;
+    
+// في حالة تكرار عندم اخذ الدواء مرتين يتم ارسال رسالة للشخص المسؤول
     dao.getAllNotificationsThatMissed().then((missedList) {
       print("misseeddddd liissssstttttt " + missedList.length.toString());
       for (int i = 0; i < missedList.length; i++) {
@@ -41,7 +45,7 @@ class CheckMissedNotifications {
             });
           }
         });
-
+// اذا كان الموعد فائت لمرة واحظه يتم ارسال إشعار اخر
         missedDao.findAllfirstMissed().then((firstMissed) {
           for (int i = 0; i < firstMissed.length; i++) {
             medicineDao
