b83a0a20bfdf70c11ebf1e12dde8ea17607df0ff
Hani Hussein <hani.mohamed.hussein@gmail.com>
2020-01-09 19:11:32 +0200

    Add comments

--- lib/dao/appointment_dao.dart
+++ lib/dao/appointment_dao.dart
@@ -3,21 +3,25 @@ import 'package:medical_reminder/entity/appointment.dart';
 
 @dao
 abstract class AppointmentDao {
+//  سحب جميع البيانات من جدول مواعيد الكشوفات
   @Query('SELECT * FROM appointment')
   Future<List<Appointment>> findAllAppointment();
 
   @Query('SELECT * FROM appointment')
   Stream<List<Appointment>> findAllAppointmentAsStream();
 
+// البحث عن موعد كشف عن طريق رقمه
   @Query('SELECT * FROM appointment WHERE id = :id')
   Future<Appointment> findAppointmentById(int id);
 
   @Query('SELECT * FROM appointment WHERE patientID = :patientID')
   Stream<List<Appointment>> findAppointmentByIdAsStream(int patientID);
 
+// إضافة موعد كشف جديد
   @insert
   Future<int> insertAppointment(Appointment appointment);
 
+// حذف موعد كشف
   @delete
   Future<void> deletAppointment(Appointment appointment);
 }
